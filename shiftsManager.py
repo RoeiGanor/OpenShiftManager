827a182a3630fb448eee943ed93121824b4388fc
joe <h255305@mvrht.net>
2019-01-19 12:15:59 +0200

    Added fuck  load of stuff including saving to file and sending email

--- shiftsManager.py
+++ shiftsManager.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+import base64
 import calendar
 import copy
 import datetime
 import io
 import json
+import mimetypes
 import os
 import random
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
@@ -13,12 +18,16 @@ from httplib2 import Http
 from oauth2client import client, file, tools
 
 SCOPES = ['https://www.googleapis.com/auth/drive',
-          'https://www.googleapis.com/auth/calendar']
+          'https://www.googleapis.com/auth/calendar',
+          'https://mail.google.com/	']
+
+global shiftsSummary
+shiftsSummary = ['תורנות בוקר', 'תורנות לילה', 'תורנות שבת']
 
 # The precentage of all days in month that it fair to have difference between
 # you and the minimal placement people
 global fairnessLevel
-fairnessLevel = 10
+fairnessLevel = 5
 
 global NIGHT_TIME
 NIGHT_TIME = 20
@@ -30,7 +39,7 @@ global unresolvedCount
 unresolvedCount = 0
 
 global SHIFT_POINTS
-SHIFT_POINTS = [1, 1.5, 2]  # Day, night, weekend
+SHIFT_POINTS = [2, 1.5, 3]  # Day, night, weekend
 
 global ITERATIONS_TIMES
 ITERATIONS_TIMES = 100
@@ -93,7 +102,7 @@ def recursiveBackTracking(day, index):
             placement[day] = temp
 
             score = getShiftScore(day)
-            peoples[i]["count"] += score
+            peoples[i]["Count"] += score
 
             if(index + 1 < len(days)):
                 index += 1
@@ -117,7 +126,7 @@ def recursiveBackTracking(day, index):
 def canBePlaced(day, people):
 
     # Check if he have constraint on this day
-    if (day.day in people["constraints"]):
+    if (day.day in people["Constraints"]):
         return False
 
     # If he has placed x days more then the lowest placed person then he cant
@@ -126,7 +135,8 @@ def canBePlaced(day, people):
     # for example: if the fairness level is 10, and there is 30 days in the month
     # then the difference between you and the lowest can be 3 days.
     minPlacement = getMinimum()
-    if(people["count"] > (daysRange[1]/fairnessLevel + minPlacement)):
+    precentage = float(fairnessLevel) / 100
+    if(people["Count"] > ((daysRange[1] * precentage) + minPlacement)):
         return False
 
     if(day.hour == NIGHT_TIME and people["canNights"] == "False"):
@@ -141,23 +151,14 @@ def canBePlaced(day, people):
 def getMinimum():
     min = daysRange[1] + 1
     for people in peoples:
-        if(people["count"] < min):
-            min = people["count"]
+        if(people["Count"] < min):
+            min = people["Count"]
     return min
 
 def getConstraintsFromDrive():
-
-    store = file.Storage('token.json')
-    creds = store.get()
-
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('drive', 'v3', http=creds.authorize(Http()))
-
+    service = getservice('drive', 'v3')
     file_id = 'X'
-    request = service.files().export_media(fileId=file_id,
-                                           mimeType='text/csv')
+    request = service.files().export_media(fileId=file_id, mimeType='text/csv')
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
     done = False
@@ -165,22 +166,30 @@ def getConstraintsFromDrive():
         status, done = downloader.next_chunk()
         print "Download %d%%." % int(status.progress() * 100)
 
-    csvf = fh.getvalue()
-    rows = csvf.split("\r\n")
+    rows = fh.getvalue().split("\r\n")
 
     COLS = rows[0].split(",")
     global CONSTRAINTS
     CONSTRAINTS = {"peoples": []}
+
+    # i for rows, each row is a person in the csv
     for i in xrange(1, len(rows)):
+
         row = rows[i]
         CONSTRAINTS["peoples"].append({})
+
+        # col for column in the csv.
         for col in xrange(len(row.split(","))):
-            if COLS[col] == "constraints":
-                CONSTRAINTS["peoples"][i - 1][COLS[col]
-                                              ] = row.split(",")[col].split(" ")
-                CONSTRAINTS["peoples"][i - 1][COLS[col]
-                                              ] = map(int, CONSTRAINTS["peoples"][i - 1][COLS[col]])
-            elif COLS[col] == "count":
+            if COLS[col] == "Constraints":
+                CONSTRAINTS["peoples"][i - 1][COLS[col]] = row.split(",")[col].split(" ")
+
+                # Check if there are no constraints.
+                if not (len(CONSTRAINTS["peoples"][i - 1][COLS[col]]) == 1 and CONSTRAINTS["peoples"][i - 1][COLS[col]][0] == ''):
+                    CONSTRAINTS["peoples"][i - 1][COLS[col]] = map(int, CONSTRAINTS["peoples"][i - 1][COLS[col]])
+                else:
+                    CONSTRAINTS["peoples"][i - 1][COLS[col]] = [0]
+
+            elif COLS[col] == "Count":
                 CONSTRAINTS["peoples"][i - 1][COLS[col]
                                               ] = int(row.split(",")[col])
             else:
@@ -189,18 +198,18 @@ def getConstraintsFromDrive():
     print(CONSTRAINTS)
 
 def sendInvite(bestRun):
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('calendar', 'v3', http=creds.authorize(Http()))
+    #store = file.Storage('token.json')
+    #creds = store.get()
+    #if not creds or creds.invalid:
+    #    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    #    creds = tools.run_flow(flow, store)
+    #service = build('calendar', 'v3', http=creds.authorize(Http()))
 
-    shiftsSummary = ['תורנות בוקר', 'תורנות לילה', 'תורנות שבת']
+    service = getservice('calendar','v3')
 
     for dayIndex in xrange(len(days) - 1):
         if bestRun['placements'][days[dayIndex]] != 'Unresolved':
-            if "@" in bestRun['placements'][days[dayIndex]]['email']:
+            if "@" in bestRun['placements'][days[dayIndex]]['Email']:
                 eventObj = {}
                 eventObj["start"] = {"dateTime": getDateString(
                     days[dayIndex]), "timeZone": "Asia/Jerusalem"}
@@ -208,7 +217,7 @@ def sendInvite(bestRun):
                 eventObj["end"] = {"dateTime": getDateString(
                     days[dayIndex + 1]), "timeZone": "Asia/Jerusalem"}
                 eventObj["attendees"] = [
-                    {"email": bestRun['placements'][days[dayIndex]]['email']}]
+                    {"Email": bestRun['placements'][days[dayIndex]]['Email']}]
                 eventObj["summary"] = shiftsSummary[getEventType(
                     days[dayIndex])]
 
@@ -234,21 +243,94 @@ def getDateString(date):
         month = "0" + month
     return str(date.year) + "-" + month + "-" + day + "T" + hour + ":00:00"
 
+def sendMesseage(bestRun,path):
+    body = ''
+    for people in bestRun["peoples"]:
+        body += people["Name"] + ":" + str(people["Count"]) + "\n"
+
+    if bestRun['unresolved'] == 0:
+        body += 'You got zero unresolved date to handle!'
+    else:
+        body += 'You got ' + str(bestRun['unresolved']) + ' unresolved date/s, you better handle them before publishing! \n'
+        for day in days:
+            if bestRun['placements'][day] == 'Unresolved':
+                body += day.strftime('%d-%m-%Y')
+
+    message = MIMEMultipart()
+    msg = MIMEText(body)
+    message['to'] = 'X'
+    message['from'] = 'X'
+    message['subject'] = str(days[0])
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(path)
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    fp = open(path, 'rb')
+    msg = MIMEBase(main_type, sub_type)
+    msg.set_payload(fp.read())
+    fp.close()
+    filename = os.path.basename(path)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+    message = {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+    service = getservice('gmail','v1')
+    message = (service.users().messages().send(userId='me', body=message).execute())
+    print 'Message Id: %s' % message['id']
+
+def createCSV(bestRun):
+    headers = 'טלפון,שם,סוג,יום,תאריך'
+    headers += '\n'
+    content = ''
+    for day in days:
+        if bestRun['placements'][day] == 'Unresolved':
+            content += 'Unresolved,Unresolved,'
+            content += shiftsSummary[getEventType(day)] + ","
+            content += day.strftime('%A') + ','
+            content += day.strftime('%d-%m-%Y') + '\n'
+        else:
+            content += bestRun['placements'][day]['Phone'] + ","
+            content += bestRun['placements'][day]['Name'] + ","
+            content += shiftsSummary[getEventType(day)] + ","
+            content += day.strftime('%A') + ','
+            content += day.strftime('%d-%m-%Y') + '\n'
+    PATH = '/tmp/'
+    PATH += days[0].strftime('%d-%m-%Y')
+    PATH += '.csv'
+    f = open(PATH,'w')
+    f.write(headers + content)
+    f.close()
+    return PATH
+
+def getservice(api,version):
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build(api, version, http=creds.authorize(Http()))
+    return service
+
 if __name__ == '__main__':
     initializeDays()
     getConstraintsFromDrive()
     times = 0
     iterations = []
-    while unresolvedCount == 0 or times < ITERATIONS_TIMES:
-        unresolvedCount = 0
+    unresolved = -1
+    while unresolved != 0 and times < ITERATIONS_TIMES:
         global peoples
         peoples = copy.deepcopy(CONSTRAINTS)
         peoples = peoples["peoples"]
         index = 0
         placement = {}
+        unresolvedCount = 0
         recursiveBackTracking(days[index], index)
         iterations.append({"placements": copy.deepcopy(
             placement), "unresolved": copy.deepcopy(unresolvedCount), "peoples": peoples})
+        unresolved = unresolvedCount
+        print times
         times += 1
 
     minUnResolved = len(days) + 1
@@ -263,6 +345,8 @@ if __name__ == '__main__':
 
     print("#######################")
     for people in bestRun["peoples"]:
-        print(people["Name"] + ":" + str(people["count"]))
+        print(people["Name"] + ":" + str(people["Count"]))
 
-    sendInvite(bestRun)
+    #sendInvite(bestRun)
+    path = createCSV(bestRun)
+    sendMesseage(bestRun,path)

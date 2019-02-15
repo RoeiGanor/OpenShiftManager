4830fc907ab728d036b148c14fcd3258395cd525
Roei Ganor <ganor.roei98@gmail.com>
2019-02-15 19:34:52 +0200

    More cleaning and function name changes

--- shiftsManager.py
+++ shiftsManager.py
@@ -49,7 +49,7 @@ SHIFT_POINTS = [int(config['SHIFT_POINTS']['DAY']), int(config['SHIFT_POINTS']['
 global ITERATIONS_TIMES
 ITERATIONS_TIMES = int(config['DEFAULT']['ITERATIONS_TIMES'])
 
-def initializeDays():
+def initialize_days():
     today = datetime.datetime.now()
 
     month = today.month
@@ -83,7 +83,7 @@ def initializeDays():
             days.append(morningShift)
             days.append(nightShift)
 
-def getShiftScore(shift):
+def get_shift_score(shift):
     score = SHIFT_POINTS[0]
     if shift.hour == NIGHT_TIME:
         score = SHIFT_POINTS[1]
@@ -92,25 +92,25 @@ def getShiftScore(shift):
 
     return score
 
-def recursiveBackTracking(day, index):
+def recursive_backtracking(day, index):
 
     i = 0
     random.shuffle(peoples)
 
     while(i < len(peoples)):
-        isPlaceable = canBePlaced(day, peoples[i])
+        isPlaceable = can_be_placed(day, peoples[i])
         if(isPlaceable):
 
             temp = copy.deepcopy(peoples[i])
             #placement[day] = peoples[i]
             placement[day] = temp
 
-            score = getShiftScore(day)
+            score = get_shift_score(day)
             peoples[i]["Count"] += score
 
             if(index + 1 < len(days)):
                 index += 1
-                answer = recursiveBackTracking(days[index], index)
+                answer = recursive_backtracking(days[index], index)
                 if answer:
                     return answer
             else:
@@ -122,7 +122,7 @@ def recursiveBackTracking(day, index):
         unresolvedCount += 1
         if(index + 1 < len(days)):
             index += 1
-            return recursiveBackTracking(days[index], index)
+            return recursive_backtracking(days[index], index)
 
     return False
 
@@ -144,7 +144,7 @@ def utility(run):
     return (3 * res) + avg_diff
 
 # Check if people can be place in this day
-def canBePlaced(day, people):
+def can_be_placed(day, people):
 
     # Check if he have constraint on this day
     if (day.day in people["Constraints"]):
@@ -155,7 +155,7 @@ def canBePlaced(day, people):
     # X is the toal days in the month divided by the fairness level.
     # for example: if the fairness level is 10, and there is 30 days in the month
     # then the difference between you and the lowest can be 3 days.
-    minPlacement = getMinimum()
+    minPlacement = get_minimum()
     precentage = float(fairnessLevel) / 100
     if(people["Count"] > ((daysRange[1] * precentage) + minPlacement)):
         return False
@@ -169,15 +169,15 @@ def canBePlaced(day, people):
     return True
 
 # Get the lowest placement count
-def getMinimum():
+def get_minimum():
     min = len(days) + 1
     for people in peoples:
         if(people["Count"] < min):
             min = people["Count"]
     return min
 
-def getConstraintsFromDrive():
-    service = getservice('drive', 'v3')
+def get_constraints_from_drive():
+    service = get_service('drive', 'v3')
     file_id = str(config['DEFAULT']['CONSTRAINTS_FILE'])
     request = service.files().export_media(fileId=file_id, mimeType='text/csv')
     fh = io.BytesIO()
@@ -218,33 +218,26 @@ def getConstraintsFromDrive():
 
     print(CONSTRAINTS)
 
-def sendInvite(bestRun):
-    #store = file.Storage('token.json')
-    #creds = store.get()
-    #if not creds or creds.invalid:
-    #    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-    #    creds = tools.run_flow(flow, store)
-    #service = build('calendar', 'v3', http=creds.authorize(Http()))
-
-    service = getservice('calendar','v3')
+def send_invite(bestRun):
+    service = get_service('calendar','v3')
 
     for dayIndex in xrange(len(days) - 1):
         if bestRun['placements'][days[dayIndex]] != 'Unresolved':
             if "@" in bestRun['placements'][days[dayIndex]]['Email']:
                 eventObj = {}
-                eventObj["start"] = {"dateTime": getDateString(
+                eventObj["start"] = {"dateTime": get_date_string(
                     days[dayIndex]), "timeZone": "Asia/Jerusalem"}
                 eventObj["end"] = {}
-                eventObj["end"] = {"dateTime": getDateString(
+                eventObj["end"] = {"dateTime": get_date_string(
                     days[dayIndex + 1]), "timeZone": "Asia/Jerusalem"}
                 eventObj["attendees"] = [
                     {"email": bestRun['placements'][days[dayIndex]]['Email']}]
-                eventObj["summary"] = shiftsSummary[getEventType(
+                eventObj["summary"] = shiftsSummary[get_event_type(
                     days[dayIndex])]
 
                 service.events().insert(calendarId='primary', body=eventObj).execute()
 
-def getEventType(day):
+def get_event_type(day):
     if day.weekday() in [4, 5]:
         return 2
     if str(day.hour) == '20':
@@ -252,7 +245,7 @@ def getEventType(day):
     if str(day.hour) == '8':
         return 0
 
-def getDateString(date):
+def get_date_string(date):
     hour = str(date.hour)
     day = str(date.day)
     month = str(date.month)
@@ -264,7 +257,7 @@ def getDateString(date):
         month = "0" + month
     return str(date.year) + "-" + month + "-" + day + "T" + hour + ":00:00"
 
-def sendMesseage(bestRun,path):
+def send_message(bestRun,path):
     body = ''
     for people in bestRun["peoples"]:
         body += people["Name"] + ":" + str(people["Count"]) + "\n"
@@ -297,26 +290,28 @@ def sendMesseage(bestRun,path):
     message.attach(msg)
     message = {'raw': base64.urlsafe_b64encode(message.as_string())}
 
-    service = getservice('gmail','v1')
+    service = get_service('gmail','v1')
     message = (service.users().messages().send(userId='me', body=message).execute())
     print 'Message Id: %s' % message['id']
 
-def createCSV(bestRun):
+def create_csv(bestRun):
     headers = 'טלפון,שם,סוג,יום,תאריך'
     headers += '\n'
     content = ''
+    
     for day in days:
         if bestRun['placements'][day] == 'Unresolved':
             content += 'Unresolved,Unresolved,'
-            content += shiftsSummary[getEventType(day)] + ","
+            content += shiftsSummary[get_event_type(day)] + ","
             content += day.strftime('%A') + ','
             content += day.strftime('%d-%m-%Y') + '\n'
         else:
             content += bestRun['placements'][day]['Phone'] + ","
             content += bestRun['placements'][day]['Name'] + ","
-            content += shiftsSummary[getEventType(day)] + ","
+            content += shiftsSummary[get_event_type(day)] + ","
             content += day.strftime('%A') + ','
             content += day.strftime('%d-%m-%Y') + '\n'
+
     PATH = '/tmp/'
     PATH += days[0].strftime('%d-%m-%Y')
     PATH += '.csv'
@@ -325,7 +320,7 @@ def createCSV(bestRun):
     f.close()
     return PATH
 
-def getservice(api,version):
+def get_service(api,version):
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -351,22 +346,22 @@ def print_finished(bestRun):
 
 def post_placement(bestRun):
 
-    path = createCSV(bestRun)
+    path = create_csv(bestRun)
     response = 'n'
     response = raw_input("commit? [y/n]")
     if response == 'y':
-        sendInvite(bestRun)
-        #path = createCSV(bestRun)
-        sendMesseage(bestRun,path)
+        send_invite(bestRun)
+        send_message(bestRun,path)
 
 if __name__ == '__main__':
-    initializeDays()
-    getConstraintsFromDrive()
+    initialize_days()
+    get_constraints_from_drive()
     times = 0
     iterations = []
     maxUtil = float("infinity")
-    #unresolved = -1
+
     bestRun = None
+
     for times in xrange(ITERATIONS_TIMES):
         global peoples
         peoples = copy.deepcopy(CONSTRAINTS)
@@ -374,25 +369,15 @@ if __name__ == '__main__':
         index = 0
         placement = {}
         unresolvedCount = 0
-        recursiveBackTracking(days[index], index)
+        recursive_backtracking(days[index], index)
         currRun = {"placements": copy.deepcopy(placement), "unresolved": copy.deepcopy(unresolvedCount), "peoples": peoples}
         utilValue = utility(currRun)
-        #iterations.append({"placements
-        #    placement), "unresolved": copy.deepcopy(unresolvedCount), "peoples": peoples})
         
         if utilValue < maxUtil:
             maxUtil = utilValue
             bestRun = currRun
 
-        #unresolved = unresolvedCount
         print times
-
-    #minUnResolved = len(days) + 1
-    #bestRun = {}
-    #for iteration in iterations:
-    #    if iteration["unresolved"] < minUnResolved:
-    #        minUnResolved = iteration["unresolved"]
-    #        bestRun = copy.deepcopy(iteration)
 
     print_finished(bestRun)
     post_placement(bestRun)
